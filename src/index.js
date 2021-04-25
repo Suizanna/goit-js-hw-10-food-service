6d1cb07257b58727523985235401a573cdd1d052
Suizanna <73129581+Suizanna@users.noreply.github.com>
2021-04-25 08:16:16 +0300

    add templates

--- src/index.js
+++ src/index.js
@@ -4,10 +4,9 @@ import './styles.css';
 
 const menuRef = document.querySelector('.js-menu'); // ссылка на рендер разметку меню
 
-
 const murkUp = itemTemplate(dishes); //itemTemplate это template-функция, которая ожидает вход аргументы
 
-menuRef.insertAdjacentHTML('beforeend', murkUp);
+menuRef.insertAdjacentHTML('beforeend', murkUp); // добавляем разметку в DOM в ul.js-menu 
 
 
 
