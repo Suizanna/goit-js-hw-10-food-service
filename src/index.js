284ebb494befbae032ecb87086cb8a2507c29989
Suizanna <73129581+Suizanna@users.noreply.github.com>
2021-04-25 07:54:17 +0300

    add

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import './styles.css';
 const menuRef = document.querySelector('.js-menu'); // ссылка на рендер разметку меню
 
 
-const murkUp = itemTemplate(dishes); //itemTemplate это template-фун-ция, которая ожидает вход аргументы
+const murkUp = itemTemplate(dishes); //itemTemplate это template-функция, которая ожидает вход аргументы
 
 menuRef.insertAdjacentHTML('beforeend', murkUp);
 
