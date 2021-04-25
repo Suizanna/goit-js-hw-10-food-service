959d6863d80ff225d5ec2ce30c331cfd2a2e6550
Suizanna <73129581+Suizanna@users.noreply.github.com>
2021-04-25 07:53:20 +0300

    add

--- src/index.js
+++ src/index.js
@@ -1,2 +1,13 @@
 import itemTemplate from './templates/dishes-cards.hbs';
 import './styles.css';
+
+
+const menuRef = document.querySelector('.js-menu'); // ссылка на рендер разметку меню
+
+
+const murkUp = itemTemplate(dishes); //itemTemplate это template-фун-ция, которая ожидает вход аргументы
+
+menuRef.insertAdjacentHTML('beforeend', murkUp);
+
+
+
